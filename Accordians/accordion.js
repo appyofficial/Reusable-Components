324f6516b7a338682496ecd740ec456e3f3c6d58
appyofficial <appy70890@gmail.com>
2020-02-17 00:01:23 +0100

    update

--- Accordians/accordion.js
+++ Accordians/accordion.js
@@ -1,14 +1,12 @@
 const accordions = document.querySelectorAll('.accordion');
-const plusSign = document.querySelectorAll('.plus');
+
 
 accordions.forEach(acc => {
     acc.addEventListener('click', () => {
         if (acc.nextElementSibling.style.display === 'block') {
             acc.nextElementSibling.style.display = 'none';
-            plusSign.innerText = "+";
         } else {
             acc.nextElementSibling.style.display = 'block';
-            plusSign.innerText = "-";
         }
     });
 });
